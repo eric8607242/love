da085caabb83267427eb603fe81b1d41f3e70d14
f74042109 <wangyiwei11259@gmail.com>
2016-11-21 14:48:23 +0800

    i

---
+++ app.js
@@ -0,0 +1,15 @@
+function move() {
+  var elem = document.getElementById("bar"); 
+  var width = 1;
+  var pgbar = setInterval(run, 10);
+  function run() {
+    if (width >= 100) {
+      clearInterval(pgbar);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+  }
+}
+
