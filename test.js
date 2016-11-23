145bb405b7f6ba415acbfb097e7e31138e5a3d4c
f74042109 <wangyiwei11259@gmail.com>
2016-11-23 20:00:59 +0800

    i

--- test.js
+++ test.js
@@ -1,29 +1,44 @@
 $(document).ready(function(){
-  $('form').submit(function(){
-    saveData();
-  }
-});
+    $('form').submit(function(){
+        saveData();
+        }
+        });
 
-function saveData(){
-  if($('#name').va()==''||$('name1').va()=='')
-  {
+    function saveData(){
+    if($('#name').va()==''||$('name1').va()=='')
+    {
     alert('fuck you\n');
-  }
-  else
-  {
+    }
+    else
+    {
     var name =  $('#name').val(); 
     var name1 = $('#name1').val();
 
     $.ajax({
-      url:"name.json";
-      type:"POST";
-      data:{name:name1},
-      success:function(data)
-      {
-        console.log("你們沒機會");
-      }
-    });
+url:"name.json";
+type:"POST";
+data:{name:name1},
+success:function(data)
+{
+console.log("你們沒機會");
+}
+});
+}
+}
+function move() {
+  var elem = document.getElementById("bar");
+  var width = 0;
+  var pgbar = setInterval(run, 10);
+  function run() {
+    if (width >= 100) {
+      clearInterval(pgbar);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
   }
 }
+
 
 
