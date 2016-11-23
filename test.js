f22beaa154e829932e71d526594425d478b28dc5
f74042109 <wangyiwei11259@gmail.com>
2016-11-23 20:22:54 +0800

    i

--- test.js
+++ test.js
@@ -26,20 +26,3 @@ console.log("你們沒機會");
 }
 }
 
-function move() {
-  var elem = document.getElementById("bar");
-  var width = 0;
-  var pgbar = setInterval(run, 10);
-  function run() {
-    if (width >= 100) {
-      clearInterval(pgbar);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-      document.getElementById("label").innerHTML = width * 1  + '%';
-    }
-  }
-}
-
-
-
