2223b7ca57884c1016fe0b9c070b549c7b2f26a6
eric8607242 <e0928021388@gmail.com>
2016-12-07 03:08:07 +0800

    eric

--- name.php
+++ name.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
 <html>
+<!DOCTYPE html>
+<html>
+<body>
+
+<link rel=stylesheet type="text/css" href="app.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="app.js"></script>
+<body bgcolor="lightblue">
+<br>
+
+</body>
+
 <?php
 $dbhost = 'localhost';
 $dbuser = 'wp2016_groupE';
@@ -11,14 +23,14 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 // Check connection
 if ($conn->connect_error) 
-    {
-      die("Connection failed: " . $conn->connect_error);
-      echo "111";
-    }
+{
+  die("Connection failed: " . $conn->connect_error);
+  echo "111";
+}
 else{
-    // prepare and bind
-    $conn -> query("INSERT INTO MyGuests(firstname,lastname)VALUE(name,name)");
-    $conn->close();
-    }
-    ?>
+  // prepare and bind
+  $conn -> query("INSERT INTO MyGuests(firstname,lastname)VALUE(name,name)");
+  $conn->close();
+}
+?>
 </html>
