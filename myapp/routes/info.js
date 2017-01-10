734976b0bdfb8904ec98f0fa615c178b35406452
f74042109 <wangyiwei11259@gmail.com>
2017-01-10 15:42:15 +0800

    i

--- myapp/routes/info.js
+++ myapp/routes/info.js
@@ -4,8 +4,21 @@ var path = require('path');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../view', 'index.html'));
+    });
+router.get('/info', function(req, res) {
     res.sendFile(path.join(__dirname, '../view', 'info.html'));
     });
+
+router.get('/abg', function(req, res) {
+    res.sendFile(path.join(__dirname, '../view', 'abg.html'));
+    });
+
+router.get('/tr', function(req, res) {
+    res.sendFile(path.join(__dirname, '../view', 'tr.html'));
+    });
+
+
 
 module.exports = router;
 /*router.post('/saveData',function(reg,res){
